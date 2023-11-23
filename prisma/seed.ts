69a348a2caf80d424ca49ea2b785a7b7ccddc8e9
Uregek <fedor.ader@bk.ru>
2023-11-23 19:36:58 +0300

    :green_heart: fix: update for prewiew build

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,46 +4,46 @@ import { slugify } from '../lib/utils'
 import data from './data.json'
 
 async function main() {
-	await db.profile.update({
-		where: {
-			id: '8422559f-8052-44ea-978a-dc312c378e63',
-		},
-		data: {
-			role: 'ADMIN',
-		},
-	})
-	// for (const { title, chips } of data) {
-	// 	await db.plot.upsert({
-	// 		where: {
-	// 			slug: slugify(title),
-	// 		},
-	// 		create: {
-	// 			title,
-	// 			slug: slugify(title),
-	// 			chips: {
-	// 				createMany: {
-	// 					data: chips.map((chip) => ({
-	// 						title: chip,
-	// 						slug: slugify(chip),
-	// 					})),
-	// 				},
-	// 			},
-	// 		},
-	// 		update: {
-	// 			title,
-	// 			slug: slugify(title),
-	// 			chips: {
-	// 				createMany: {
-	// 					data: chips.map((chip) => ({
-	// 						title: chip,
-	// 						slug: slugify(chip),
-	// 					})),
-	// 					skipDuplicates: true,
-	// 				},
-	// 			},
-	// 		},
-	// 	})
-	// }
+	// await db.profile.update({
+	// 	where: {
+	// 		username: 'uregek',
+	// 	},
+	// 	data: {
+	// 		role: 'ADMIN',
+	// 	},
+	// })
+	for (const { title, chips } of data) {
+		await db.plot.upsert({
+			where: {
+				slug: slugify(title),
+			},
+			create: {
+				title,
+				slug: slugify(title),
+				chips: {
+					createMany: {
+						data: chips.map((chip) => ({
+							title: chip,
+							slug: slugify(chip),
+						})),
+					},
+				},
+			},
+			update: {
+				title,
+				slug: slugify(title),
+				chips: {
+					createMany: {
+						data: chips.map((chip) => ({
+							title: chip,
+							slug: slugify(chip),
+						})),
+						skipDuplicates: true,
+					},
+				},
+			},
+		})
+	}
 }
 main()
 	.then(async () => {
